c75f2d9965b6269cd72a31b60771d96196a36031
Carlos Kayser <android.app.ch@gmail.com>
2020-03-25 15:31:51 -0300

    Initial Commit - Simple HTTP Server

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var cors = require('cors');
+
+var app = express();
+
+app.get('/hello', function(req, res) {
+    res.send({
+        message: 'hello'
+    })
+})
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(3333);
