e43a9db375a3f92dee16d16cc0d4762cdab8db46
Carlos Kayser <android.app.ch@gmail.com>
2020-04-07 16:15:39 -0300

    Dockerfile

--- server.js
+++ server.js
@@ -3,13 +3,27 @@ var cors = require('cors');
 
 var app = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.get('/hello', function(req, res) {
     res.send({
         message: 'Hello World'
     })
 })
 
-app.use(cors());
-app.use(express.json());
+app.post('/sum', function(req, res) {
+
+    if (req.body.num1 && req.body.num2) {
+        res.send({
+            result: Number(req.body.num1) + Number(req.body.num2)
+        })
+    } else {
+        res.status(400).send({
+            message: "num1 and num2 can't be null"
+        })
+    }
+
+})
 
 app.listen(3333);
